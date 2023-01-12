a5dc7ee6c97d011ae582434af6479f16b09789c3
Nikita <32540777+nivanown@users.noreply.github.com>
2023-01-12 13:35:56 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,150 @@
+﻿/*- tabs -*/
+const tabs = document.querySelectorAll('.tabs__nav li')
+const tabContents = document.querySelectorAll('.tabs__item')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+/*- gl-select -*/
+function checkboxDropdown(el) {
+  var $el = $(el)
+
+  function updateStatus(label, result) {
+    if(!result.length) {
+      label.html('Все');
+    }
+  };
+  
+  $el.each(function(i, element) {
+    var $list = $(this).find('.gl-select__dropdown'),
+      $label = $(this).find('.gl-select__text'),
+      $checkAll = $(this).find('.check-all'),
+      $inputs = $(this).find('.check'),
+      defaultChecked = $(this).find('input[type=checkbox]:checked'),
+      result = [];
+    
+    updateStatus($label, result);
+    if(defaultChecked.length) {
+      defaultChecked.each(function () {
+        result.push($(this).next().text());
+        $label.html(result.join(", "));
+      });
+    }
+    
+    $label.on('click', ()=> {
+      $(this).toggleClass('open');
+    });
+
+    $checkAll.on('change', function() {
+      var checked = $(this).is(':checked');
+      var checkedText = $(this).next().text();
+      result = [];
+      if(checked) {
+        result.push(checkedText);
+        $label.html(result);
+        $inputs.prop('checked', false);
+      }else{
+        $label.html(result);
+      }
+        updateStatus($label, result);
+    });
+
+    $inputs.on('change', function() {
+      var checked = $(this).is(':checked');
+      var checkedText = $(this).next().text();
+      if($checkAll.is(':checked')) {
+        result = [];
+      }
+      if(checked) {
+        result.push(checkedText);
+        $label.html(result.join(", "));
+        $checkAll.prop('checked', false);
+      }else{
+        let index = result.indexOf(checkedText);
+        if (index >= 0) {
+          result.splice(index, 1);
+        }
+        $label.html(result.join(", "));
+      }
+      updateStatus($label, result);
+    });
+
+    $(document).on('click touchstart', e => {
+      if(!$(e.target).closest($(this)).length) {
+        $(this).removeClass('open');
+      }
+    });
+  });
+};
+
+checkboxDropdown('.gl-select');
+
+/*- gl-select__scroll -*/
+const swiper = new Swiper(".gl-select__scroll", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  freeMode: true,
+  scrollbar: {
+    el: ".gl-select__scroll .swiper-scrollbar",
+  },
+  mousewheel: true,
+});
+
+/*- phone-input -*/
+[].forEach.call(document.querySelectorAll('.phone-input'), function (input) {
+    let keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+7 (___) ___-__-__",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            newValue = matrix.replace(/[_\d]/g, function (a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+            });
+        i = newValue.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            newValue = newValue.slice(0, i);
+        }
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function (a) {
+                return "\\d{1," + a.length + "}";
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = newValue;
+        if (event.type == "blur" && this.value.length < 5) this.value = "";
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+});
+
+/*- info-panel__item -*/
+let buttons = document.getElementsByClassName("info-panel__item");
+for(let i = 0; i < buttons.length; i++) {
+  buttons.item(i).addEventListener("click", doClickButton);
+}
+
+function doClickButton(e) {
+  toggleButton(e.target);
+}
+
+function toggleButton(button) {
+  button.classList.toggle('active');
+}
